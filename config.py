42894f3e13d06e4197c43ccad6d33dffc9841f38
Michael <michaelo_@hotmail.com>
2025-06-04 16:57:51 +0200

    updated the files to remove sensitive information and include requirements

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ load_dotenv()
 
 CLIENT_ID = os.environ.get("TWITCH_CLIENT_ID")
 CLIENT_SECRET = os.environ.get("TWITCH_CLIENT_SECRET")
-REDIRECT_URI = os.environ.get("TWITCH_REDIRECT_URI")  # e.g., https://your-app.onrender.com/callback
+REDIRECT_URI = os.environ.get("TWITCH_REDIRECT_URI")
 SCOPES = "analytics:read:games analytics:read:extensions user:read:email"
