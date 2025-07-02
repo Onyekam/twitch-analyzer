d83247bffc758f3ff13c56b443fb3eced1641d21
Michael <michaelo_@hotmail.com>
2025-07-02 16:29:33 +0200

    added frontend boilerplate with react

---
+++ frontend/vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react' // or react, etc.
+import path from 'path'
+
+export default defineConfig({
+  plugins: [react()], // or your framework plugin
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+})
